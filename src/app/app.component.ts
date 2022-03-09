6495fc5755fad08aad2b0b456581198295e7fb60
vongminhtan <vongminhtan97@gmail.com>
2022-03-09 22:59:26 +0700

    feat: request push notification permission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwPush} from "@angular/service-worker";
+import {NewsletterService} from "./newsletter.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-notification';
+
+  readonly VAPID_PUBLIC_KEY = "BPGjZKgbrhOAxo6rL3X6OM8yrp21IU7yLhKicERg1wTRE-44VxxHRIh8k7rm0TTZFmnD8NfMUttuOMfX2LJRHT8";
+
+  constructor(
+    private swPush: SwPush,
+    private newsletterService: NewsletterService) {}
+
+  subscribeToNotifications() {
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {NewsletterService} from "./newsletter.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
-  providers: [],
+  providers: [NewsletterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
